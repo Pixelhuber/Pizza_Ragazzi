053db2cf57e9cc371249e2dee44f7150c63f9b08
Andi Balser <andreas.balser@stud-mail.uni-wuerzburg.de>
2020-12-10 10:26:59 +0100

    code restructuring and documentation

--- public/javascripts/functions_profile-page.js
+++ public/javascripts/functions_profile-page.js
@@ -1,16 +1,20 @@
 
 function displayAchievements() {
-    var allAchievements = [];
+
+    // Create Achievement-Array
+    const allAchievements = [];
     for (i = 0; i <= 30; i++){
-        allAchievements.push('Achievement '+i);
+        allAchievements.push('Achievement '+ i);
     }
 
+    // Display Achievements
     allAchievements.forEach(function (item, index, array) {
         console.log(index);
-        var div = document.createElement('div');
-        div.textContent = item;
-        div.setAttribute('class', 'achievementBox');
-        document.body.appendChild(div);
+        const achievementDiv = document.createElement('div');
+        achievementDiv.textContent = item;
+        achievementDiv.setAttribute('class', 'achievementBox');
+        document.getElementById("achievements_table").appendChild(achievementDiv);
+        document.getElementById("achievements_table")
     });
 }
 
@@ -19,42 +23,54 @@ $(function () {
     //edit the profile (username, picture) (email and password could be added in the future)
     $("#profileButton").on('click', function () {
 
-        if ($(this).text() === "Edit Profile") {
+        // CONSTANTS -----------------------------------
+        const profileButton = $(this);
+        const usernameField = $("#username");
+        const selectFile = $("#p-image");
+        // ---------------------------------------------
 
-            var username = $("#username").text();
-            //hier nutze ich den style tag nur, weil er aus dem css sheet irgendwie nicht geladen wurde
-            var usernameInputField = "<input style='font-size: 60px' name=\"new_username\" value=" + username + ">";
-            $("#username").html(usernameInputField);
-            $(this).text("Save Profile");
+        if (profileButton.text() === "Edit Profile") {
+            const usernameText = usernameField.text();
 
-            $("#p-image").html("<input style='font-size: 22px' id=\"file-upload\" type=\"file\" accept=\"image/*\"/>")
+            // change usernameField into an inputField
+            const usernameInputField = "<input style='font-size: 60px' name=\"new_username\" value=" + usernameText + ">"; //hier nutze ich den style tag nur, weil er aus dem css sheet irgendwie nicht geladen wurde
+            usernameField.html(usernameInputField);
+
+            // display field to change profile-picture
+            const selectFileButton = "<input style='font-size: 22px' id=\"file-upload\" type=\"file\" accept=\"image/*\"/>"
+            selectFile.html(selectFileButton);
             $("#file-upload").on('change', function(){
                 readURL(this);
             });
 
-        }else if ($(this).text() === "Save Profile"){
 
-            var newUsername = document.forms["userForm"]["new_username"].value;
+            profileButton.text("Save Profile");
+
+
+        } else if (profileButton.text() === "Save Profile"){
+
+            const newUsername = document.forms["userForm"]["new_username"].value;
             if (newUsername === "") {
                 alert("Your Username should not be empty");
             }else {
-                $("#username").html(newUsername);
-                $(this).text("Edit Profile");
-                $("#p-image").html("");
+                usernameField.html(newUsername);
+                selectFile.html("");
+
+                profileButton.text("Edit Profile");
             }
         }
     });
 
     //function to upload pictures
-    var readURL = function(input) {
+    function readURL(input) {
         if (input.files && input.files[0]) {
-            var reader = new FileReader();
 
+            const reader = new FileReader();
             reader.onload = function (e) {
                 $('#profile-picture').attr('src', e.target.result);
             }
 
-            reader.readAsDataURL(input.files[0]);
+            reader.readAsDataURL(input.files[0]); //Actually change the picture
         }
     }
 });
