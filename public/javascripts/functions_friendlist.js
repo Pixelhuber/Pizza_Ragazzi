3012b6cc5ab05868911bc5c119014c304758e8c8
Lennard Rüffert <lennard.rueffert@stud-mail.uni-wuerzburg.de>
2021-01-25 22:19:31 +0100

    Freundesliste wird nun angezeigt, jedoch vorläufig mit default werten da Datenbank anscheinend nicht will

--- public/javascripts/functions_friendlist.js
+++ public/javascripts/functions_friendlist.js
@@ -1,21 +1,29 @@
 function getFriendsData() {
-    fetch("/highscore/getFriendsData")
+    fetch("/getFriendsData")
         .then(result => result.json())
         .then(result => createFriendlist(result));
 }
 
 function createFriendlist(data){
-    var list = document.createElement(`ul`)
-    list.className = `friend-list`
+    var list = document.createElement(`ul`);
+    list.className = `friend-list`;
     data.forEach(function (item,index) {
-        var friend = document.createElement(`li`)
-        friend.className = `friend`
-        
-        var name = document.createElement(`div`)
-        name.className = `name`
+        var friend = document.createElement(`li`);
+        friend.className = `friend`;
+        var image= document.createElement(`img`)
+        image.src = 'https://i.imgur.com/nkN3Mv0.jpg'
+        var name = document.createElement(`div`);
+        name.className = `name`;
         name.innerHTML = item;
-        friend.appendChild(name)
-        list.appendChild(friend)
+        friend.appendChild(image)
+        friend.appendChild(name);
+        list.appendChild(friend);
     });
-    document.body.appendChild(list)
+    document.getElementById("container_friends").append(list);
+}
+
+function defaultList(){
+    var test = ["Andres Perez","Leah Slaten"];
+    createFriendlist(test);
+
 }
