88a49e98b87628de1f3c400043bc336753a61870
Daniel <daniel.schuhmann@stud-mail.uni-wuerzburg.de>
2021-02-14 12:23:16 +0100

    clickCard für beide Karten

--- public/javascripts/functions_memory.js
+++ public/javascripts/functions_memory.js
@@ -28,8 +28,7 @@ class MemoryCard{
 
         card.gameElement.text.innerHTML = this.ingredient;
 
-        card.gameElement.text.innerHTML = this.ingredient;
-
+        card.gameElement.text.style.visibility = "hidden"
         card.gameElement.setAttribute('id', this.id);
 
         /*card.gameElement.setAttribute('onmousedown', card.turnAround.bind(this));*/
@@ -51,6 +50,9 @@ class MemoryCard{
         card.gameElement.setAttribute('class', 'memoryCard');
 
         card.gameElement.text.innerHTML = this.fact;
+        card.gameElement.text.style.visibility = "hidden"
+
+        this.id = -this.id
 
         card.gameElement.setAttribute('id', this.id);
 
@@ -64,28 +66,23 @@ class MemoryCard{
 
 class NameCard extends MemoryCard{
 
-    constructor(memoryCard) {
+    id;
+
+    constructor(memoryCard,id) {
         super(memoryCard.ingredient);
-        this.name = name;
-    }
-
-    turnAround(){
-        this.gameElement.text.innerHTML = "Hallo";
-
+        this.id = id;
     }
 
 }
 
 class FactCard extends MemoryCard{
 
-    constructor(memoryCard, fact) {
+    id;
+
+    constructor(memoryCard, fact, id) {
         super(memoryCard.ingredient, memoryCard.fact);
         this.fact = fact;
-    }
-
-    turnAround(){
-        this.gameElement.text.innerHTML = "Hallo";
-
+        this.id = -id;
     }
 
 }
@@ -96,19 +93,15 @@ function loadMemoryCards(){                                                     
 
     for (let i = 0; i < ingredientList.length; i++) {
         let fact = getIngredientFact(ingredientList[i], getCurrentTier())
-        let memoryCard = new MemoryCard(ingredientList[i],fact,i).createCards()
+        let memoryCard = new MemoryCard(ingredientList[i],fact,i+1).createCards()
 
     }
 
     //onClick müsste eigentlich für NameCard und FactCard gehen, da beide die gleiche ID haben
 
-    for (let i = 0; i < ingredientList.length; i++) {
-        let  theBox = document.getElementById(i.toString());
-        theBox.onclick = function(evt) {
-            toggle(theBox);
-        }
+    clickCard(ingredientList);
 
-    }
+
 
 }
 
@@ -120,8 +113,40 @@ function toggle(card){
         card.text.style.visibility = "visible"
     }
 
+
 }
 
+function clickCard(ingredientList){
+    for (let i = -ingredientList.length; i <= ingredientList.length; i++) {
+        if(!(i === 0)){
+            let  firstCard = document.getElementById(i.toString());
+            firstCard.onclick = function(evt) {
+                toggle(firstCard);
+                for (let j = -ingredientList.length; j <= ingredientList.length; j++) {
+                    if(!(j === 0)){
+                        let secondCard = document.getElementById(j.toString());
+                        secondCard.onclick = function (evt) {
+                            toggle(secondCard);
+                            if ((parseInt(firstCard.id) + parseInt(secondCard.id)) === 0) {
+                                firstCard.remove();
+                                secondCard.remove();
+                            } else {
+                                setTimeout(function () {
+                                    if (firstCard.text.style.visibility === "visible" && secondCard.text.style.visibility === "visible")
+                                        toggle(firstCard);
+                                        toggle(secondCard);
+                                }, 1500);
+                        }
+
+                    }
+                }
+            }
+        }
+        }
+
+    }
+
+}
 
 // TODO: Get Tier and Ingredientlist from Database --------------------
 
@@ -136,8 +161,8 @@ function getNextTierIngredients(tier){
 function getIngredientFact(ingredient, currentTier){
     switch (ingredient){
         case "impasto":
-            if (currentTier == 1){
-                return "es gibt über 300 knettechniken"
+            if (currentTier === 1){
+                return "Fakt über Teig"
             }
         break;
 
