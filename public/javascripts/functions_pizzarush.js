8e013a612b2c0d20611d0c3e3068a1488f69768b
Lennard Rüffert <lennard.rueffert@stud-mail.uni-wuerzburg.de>
2021-03-11 12:48:19 +0100

    background music lautstärke

--- public/javascripts/functions_pizzarush.js
+++ public/javascripts/functions_pizzarush.js
@@ -936,7 +936,7 @@ async function getAvailablePizzas() {
 async function loadGameElements() {
     var music = new Audio('/assets/sounds/tarantella_napoletana.mp3');
     music.loop=true;
-    music.volume=0.005;
+    music.volume=0.01;
     await music.play();
     await setupAvailableIngredients();
     await setupAvailablePizzas();
